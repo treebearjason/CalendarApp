8b3cf09cdaea15e84828af3cd5485a67f94bec61
jasonwong <jason.wong@hunt.social>
2016-12-06 18:09:13 +0800

    get friend list intial commit

---
+++ app/src/main/java/com/jwson/calendarapp/adapter/ContactListAdapter.java
@@ -0,0 +1,14 @@
+package com.jwson.calendarapp.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.widget.ArrayAdapter;
+import android.widget.SimpleCursorAdapter;
+
+/**
+ * Created by user on 12/6/2016.
+ */
+
+public class ContactListAdapter  {
+
+}
